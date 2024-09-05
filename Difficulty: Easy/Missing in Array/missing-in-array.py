6cf953a608a6acbc74add3a9b368a7578adfc95f
Jaya Sri <144419863+Sri1171@users.noreply.github.com>
2024-09-05 11:04:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+    
+    # Note that the size of the array is n-1
+    def missingNumber(self, n, arr):
+        
+        # code here
+        arr.sort()
+        for i in range(1,n):
+            if arr[i-1]!=i:
+                return i
+        return n
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int(input())
+for _ in range(0, t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    s = Solution().missingNumber(n, arr)
+    print(s)
+
+# } Driver Code Ends
