fc52880c7b0d178485eda57d866b7c2f110d8553
Jaya Sri Durga Perumalla <22A91A0547@aec.edu.in>
2025-05-11 11:05:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        vector<int>ans;
+        for(int i=0;i<arr.size();i++){
+            int check=0;
+            for(int j=i;j<arr.size();j++){
+                check+=arr[j];
+                ans.push_back(check);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        
+        return ans[ans.size()-k];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        int res = obj.kthLargest(arr, k);
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
