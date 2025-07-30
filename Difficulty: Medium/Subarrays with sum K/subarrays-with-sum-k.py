77e0a2e6c8c5730654d341789d67de0b1a85e876
Jaya Sri Durga Perumalla <22A91A0547@aec.edu.in>
2025-07-30 21:00:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
@@ -1,9 +1,11 @@
 #User function Template for python3
 
 class Solution:
-    def countSubarrays(self, arr, k):
+    def cntSubarrays(self, arr, k):
         # code here
         l=[]
+        if k==0:
+            return 0
         summ=0
         d={0:1}
         ans=0
@@ -15,42 +17,11 @@ class Solution:
                 d[summ]+=1
             if summ-k in d:
                 ans+=d[summ-k]
+                
+        # print(d)
         return ans
             
                 
 
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import atexit
-import io
-import sys
-from collections import deque
-
-#Contributed by : Nagendra Jha
-
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-
-@atexit.register
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        k = int(input())
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        res = ob.countSubarrays(arr, k)
-        print(res)
-        print("~")
-
-# } Driver Code Ends
