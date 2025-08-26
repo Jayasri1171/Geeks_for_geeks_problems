fa1a5aea7d6372a60d967918b4908803b941c64b
Jaya Sri Durga Perumalla <22A91A0547@aec.edu.in>
2025-08-26 22:29:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if a String is Subsequence of Other/check-if-a-string-is-subsequence-of-other.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubSeq(self, s1, s2):
+        # code here
+        i=0
+        j=0
+        while(i<len(s1) and j<len(s2)):
+            if s1[i]==s2[j]:
+                i+=1
+                j+=1
+            else:
+                j+=1
+        if(i==len(s1)):
+            return True
+        return False
+        
