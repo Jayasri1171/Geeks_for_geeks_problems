352c91f8f725a2e5ceae11051e287b8d3f6e54d5
Jaya Sri Durga Perumalla <22A91A0547@aec.edu.in>
2025-10-23 15:03:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.py
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.py
@@ -1,10 +1,3 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-from typing import List
-
-
-# } Driver Code Ends
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         # Your code here
@@ -34,26 +27,3 @@ class Solution:
             
         # print(m)
         return ans
-
-#{ 
- # Driver Code Starts.
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        t -= 1
-        k = int(input())
-        n = int(input())
-        points = []
-        for _ in range(n):
-            x, y = map(int, input().split())
-            points.append([x, y])
-        
-        solution = Solution()
-        ans = solution.kClosest(points, k)
-        ans.sort()
-        for point in ans:
-            print(point[0], point[1])
-        print("~")
-
-# } Driver Code Ends
